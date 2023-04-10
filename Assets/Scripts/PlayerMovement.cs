c7884b66004950572d3682f42ebc32951221d27a
GamesDoneWr0ng <ask@flaten.no>
2023-04-10 18:38:40 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private Rigidbody rb;
-    private 
+    private Collider groundCheck;
     public float playerSpeed = 2.0f;
     public float jumpHeight = 1.0f;
 
@@ -16,11 +16,26 @@ public class PlayerMovement : MonoBehaviour
     {
         controller = gameObject.AddComponent<CharacterController>();
         rb = gameObject.AddComponent<Rigidbody>();
+        groundCheck = gameObject.AddComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ()
+        // jump
+        if (groundCheck.isTrigger && Input.GetButtonDown("Jump")) {
+            playerVelocity.y += jumpHeight
+        }
+
+        // walk
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        controller.Move(playerVelocity)
     }
 }
