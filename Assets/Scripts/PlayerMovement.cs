5f7265014699d82860d50de9b4751a130ed2b4c0
GamesDoneWr0ng <ask@flaten.no>
2023-04-15 17:02:49 +0200

    Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,46 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController controller;
     public Vector3 playerVelocity;
     public float playerSpeed = 2.0f;
     public float jumpHeight = 1.0f;
-    public bool isGrounded = true;
     public double gravity = -9.81;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
+        controller.skinWidth = 0.0001f;
     }
  
     // Update is called once per frame
     void Update()
     {
+        // gravity
+        playerVelocity.y += (float) gravity * Time.deltaTime;
+
+        if (controller.isGrounded && playerVelocity.y < 0) {
+            playerVelocity.y = 0f;
+        }
+
         // jump
-        if (isGrounded && Input.GetButtonDown("Jump")) {
-            playerVelocity.y += jumpHeight;
+        if (controller.isGrounded && Input.GetButton("Jump")) {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * (float)gravity);
         }
 
         // walk
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        controller.Move(move.normalized * Time.deltaTime * playerSpeed);
+        
+        if (move != Vector3.zero) {
             gameObject.transform.forward = move;
         }
 
-        controller.Move(playerVelocity);
-    }
-
-    void OnCollisionStay(Collision collisionInfo)
-    {
-        // Debug-draw all contact points and normals
-        foreach (ContactPoint contact in collisionInfo.contacts)
-        {
-            Debug.DrawRay(contact.point, contact.normal * 10, Color.blue);
-            Debug.Log($"a");
-        }
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
